4c7a7f43b4237b9c8ff45ecdaf4fc6eb0d73dfcc
Lazolia Buzuzi <zobuzuzi@gmail.com>
2023-03-28 19:34:02 -0700

    changed placement of precision to before size

--- functions.c
+++ functions.c
@@ -8,7 +8,6 @@
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
  * @width: Width
- * @size: Size specifier
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
@@ -28,8 +27,8 @@ int print_char(va_list types, char buffer[],
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
  * @width: Width
- * @size: Size specifier
  * @precision: Precision specification
+ * @size: Size specifier
  * Return: Number of chars printed
  */
 int print_string(va_list types, char buffer[],
